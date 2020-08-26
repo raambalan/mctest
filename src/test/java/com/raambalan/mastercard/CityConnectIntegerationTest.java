696e1f46f1e5dc2ec2e1fe4f5c8765e382a7c89b
61267@hrmc.com <61267@hrmc.com>
2020-08-26 19:32:12 -0400

    Fixed integration test-with testTemplate and mockMvc

--- src/test/java/com/raambalan/mastercard/CityConnectIntegerationTest.java
+++ src/test/java/com/raambalan/mastercard/CityConnectIntegerationTest.java
@@ -1,17 +1,27 @@
 package com.raambalan.mastercard;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URLEncoder;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CityConnectIntegerationTest {
 
@@ -26,36 +36,55 @@ public class CityConnectIntegerationTest {
                 .build();
     }
 
+    @LocalServerPort
+    private int port;
+    TestRestTemplate restTemplate = new TestRestTemplate();
+    HttpHeaders headers = new HttpHeaders();
 
     @Test
-    void directPath() throws Exception {
-        this.mockMvc.perform(
+    public void testDirectPathWithRandomPort() throws Exception {
+        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(createURLWithPort("/connected"))
+                .queryParam("origin", URLEncoder.encode("New York", "UTF-8"))
+                .queryParam("destination", URLEncoder.encode("Boston", "UTF-8"));
+        ResponseEntity<String> response = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, entity, String.class);
+        String actual = response.getBody().trim();
+        Assertions.assertTrue(actual.contains("Yes"));
+    }
+
+    private String createURLWithPort(String uri) {
+        return "http://localhost:" + port + uri;
+    }
+
+    @Test
+    void directPathWitMockMvc() throws Exception {
+        Assertions.assertTrue(this.mockMvc.perform(
                 get("/connected")
                         .param("origin", "New York")
                         .param("destination", "Boston"))
-                .andReturn().getResponse().getContentAsString().equalsIgnoreCase("Yes");
+                .andReturn().getResponse().getContentAsString().equalsIgnoreCase("Yes"));
         ;
 
     }
 
     @Test
-    void multiCityPath() throws Exception {
-        this.mockMvc.perform(
+    void multiCityPathWitMockMvc() throws Exception {
+        Assertions.assertTrue(this.mockMvc.perform(
                 get("/connected")
-                        .param("origin", "Philadelphia York")
+                        .param("origin", "Philadelphia")
                         .param("destination", "Boston"))
-                .andReturn().getResponse().getContentAsString().equalsIgnoreCase("Yes");
+                .andReturn().getResponse().getContentAsString().equalsIgnoreCase("Yes"));
         ;
 
     }
 
     @Test
-    void pathNotFound() throws Exception {
-        this.mockMvc.perform(
+    void pathNotFoundWitMockMvc() throws Exception {
+        Assertions.assertTrue(this.mockMvc.perform(
                 get("/connected")
                         .param("origin", "Trenton")
                         .param("destination", "Boston"))
-                .andReturn().getResponse().getContentAsString().equalsIgnoreCase("No");
+                .andReturn().getResponse().getContentAsString().equalsIgnoreCase("No"));
         ;
     }
 }
